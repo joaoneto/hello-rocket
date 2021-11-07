4b2f3291fb9d17a43b430a2650b30053b39c3893
João Neto <joaopintoneto@gmail.com>
2021-11-07 15:22:25 -0300

    add fixed max_pool_size for mongodb client

--- src/lib/db.rs
+++ src/lib/db.rs
@@ -1,7 +1,8 @@
 use mongodb::{options::ClientOptions, Client};
 
 pub async fn get_connection() -> Client {
-    let client_options = ClientOptions::parse("mongodb://localhost:27017").await.unwrap();
+    let mut client_options = ClientOptions::parse("mongodb://localhost:27017").await.unwrap();
+    client_options.max_pool_size = Some(10);
     let client = Client::with_options(client_options).unwrap();
 
     client
